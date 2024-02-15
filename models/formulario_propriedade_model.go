10b61b524d86279d2e9ce289a767c654d1b759a4
necojr <necoengenheiro@gmail.com>
2024-02-15 16:13:51 -0300

    refactore: alteração propriedade do formulario

--- models/formulario_propriedade_model.go
+++ models/formulario_propriedade_model.go
@@ -3,11 +3,9 @@ package models
 import "github.com/GeoinovaDev/app-geoinova-entity/entity"
 
 type FormularioPropriedadeModel struct {
-	Id           uint
-	Nome         string
-	Field        string
-	Tipo         string
-	FormularioId uint `gorm:"column:formulario_id"`
+	Id   uint
+	Nome string
+	Tipo string
 }
 
 func (m *FormularioPropriedadeModel) TableName() string {
@@ -18,7 +16,6 @@ func (m *FormularioPropriedadeModel) ToEntity() *entity.FormularioPropriedade {
 	return entity.
 		NewFormularioPropriedadeBuilder(m.Id).
 		WithNome(m.Nome).
-		WithField(m.Field).
 		WithTipo(entity.FormularioPropriedadeTipo(m.Tipo)).
 		Build()
 }
